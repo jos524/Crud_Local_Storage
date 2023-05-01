96042b74dc300e1b0b448c6db0c4d309547fcff0
José Maria de Mattos Netto <107483809+jos524@users.noreply.github.com>
2023-05-01 20:57:22 -0300

    Fix:  Alterado o style de todos os arquivos, Arrumado nome do método de atualizar pessoa

--- script.js
+++ script.js
@@ -1,136 +1,137 @@
 document.onload = mostraPessoas();
 
 function validaFormulario() {
-  const nome = document.getElementById("nome").value;
-  const endereco = document.getElementById("endereco").value;
-  const idade = document.getElementById("idade").value;
-  const email = document.getElementById("email").value;
+    const nome = document.getElementById("nome").value;
+    const endereco = document.getElementById("endereco").value;
+    const idade = document.getElementById("idade").value;
+    const email = document.getElementById("email").value;
 
-  if (
-    nome.length == 0 ||
-    endereco.length == 0 ||
-    idade.length == 0 ||
-    email.length == 0
-  ) {
-    alert("Todos os campos devem ser preenchidos!");
-    return false;
-  }
+    if (
+        nome.length == 0 ||
+        endereco.length == 0 ||
+        idade.length == 0 ||
+        email.length == 0
+    ) {
+        alert("Todos os campos devem ser preenchidos!");
+        return false;
+    }
 
-  if (idade <= 0 || isNaN(idade)) {
-    alert("A idade precisa ser positiva e maior que 0!");
-    return false;
-  }
+    if (idade <= 0 || isNaN(idade)) {
+        alert("A idade precisa ser positiva e maior que 0!");
+        return false;
+    }
 
-  if (!email.includes("@") || !email.includes(".com")) {
-    alert("O email precisa ser um email valido!");
-    return false;
-  }
+    if (!email.includes("@") || !email.includes(".com")) {
+        alert("O email precisa ser um email valido!");
+        return false;
+    }
 
-  return true;
+    return true;
 }
 
 function mostraPessoas() {
-  const pessoasDb = getPessoasDoLocalStorage();
+    const pessoasDb = getPessoasDoLocalStorage();
 
-  let html = ``;
+    let html = ``;
 
-  pessoasDb.forEach(function (pessoa, index) {
-    html += `
-      <tr>
-      <td> ${pessoa.nome} </td>
-      <td> ${pessoa.idade} </td>
-      <td> ${pessoa.endereco} </td>
-      <td> ${pessoa.email} </td>
-      <td> 
-      <button onclick="deletaPessoa(${index})" class= 'btn btn-danger' id='deletar'> Deletar </button> 
-      <button onclick=mostraDadosParaAtualizacao(${index}) class= 'btn btn-warning' id='atualizar'> Atualizar </button>
-      </td>
-      </tr>
+    pessoasDb.forEach(function (pessoa, index) {
+        html += `
+        <tr>
+        <td> ${pessoa.nome} </td>
+        <td> ${pessoa.idade} </td>
+        <td> ${pessoa.endereco} </td>
+        <td> ${pessoa.email} </td>
+        <td> 
+        <button onclick="deletaPessoa(${index})" class= 'btn btn-danger' id='deletar'> Deletar </button> 
+        <button onclick=mostraDadosParaAtualizacao(${index}) class= 'btn btn-warning' id='atualizar'> Atualizar </button>
+        </td>
+        </tr>
     `;
-  });
+    });
 
-  document.querySelector("#tabela tbody").innerHTML = html;
+    document.querySelector("#tabela tbody").innerHTML = html;
 }
 
 function adicionarPessoa() {
-  if (validaFormulario()) {
-    const nome = document.getElementById("nome").value;
-    const idade = document.getElementById("idade").value;
-    const endereco = document.getElementById("endereco").value;
-    const email = document.getElementById("email").value;
+    if (validaFormulario()) {
+        const nome = document.getElementById("nome").value;
+        const idade = document.getElementById("idade").value;
+        const endereco = document.getElementById("endereco").value;
+        const email = document.getElementById("email").value;
 
-    const pessoasDb = getPessoasDoLocalStorage();
+        const pessoasDb = getPessoasDoLocalStorage();
 
-    pessoasDb.push({
-      nome: nome,
-      idade: idade,
-      endereco: endereco,
-      email: email,
-    });
+        pessoasDb.push({
+            nome: nome,
+            idade: idade,
+            endereco: endereco,
+            email: email,
+        });
 
-    localStorage.setItem("pessoas", JSON.stringify(pessoasDb));
+        localStorage.setItem("pessoas", JSON.stringify(pessoasDb));
 
-    mostraPessoas();
-    limpaFormulario();
-  }
+        mostraPessoas();
+        limpaFormulario();
+    }
 }
 
 function deletaPessoa(index) {
-  const pessoasDb = getPessoasDoLocalStorage();
+    const pessoasDb = getPessoasDoLocalStorage();
 
-  pessoasDb.splice(index, 1);
-  localStorage.setItem("pessoas", JSON.stringify(pessoasDb));
+    pessoasDb.splice(index, 1);
+    localStorage.setItem("pessoas", JSON.stringify(pessoasDb));
 
-  mostraPessoas();
+    mostraPessoas();
 }
 
-function mostraDadosParaAtualizacao(index) {
-  document.getElementById("btnAdicionar").style.display = "none";
-  document.getElementById("deletar").style.display = "none";
-  document.getElementById("atualizar").style.display = "none";
-  document.getElementById("btnAtualizar").style.display = "block";
-  document.getElementById("btnCancelar").style.display = "block";
+function atualizaPessoa(index) {
+    document.getElementById("btnAdicionar").style.display = "none";
+    document.getElementById("deletar").style.display = "none";
+    document.getElementById("atualizar").style.display = "none";
+    document.getElementById("btnAtualizar").style.display = "block";
+    document.getElementById("btnCancelar").style.display = "block";
 
-  const pessoasDb = getPessoasDoLocalStorage();
+    const pessoasDb = getPessoasDoLocalStorage();
 
-  document.getElementById("nome").value = pessoasDb[index].nome;
-  document.getElementById("idade").value = pessoasDb[index].idade;
-  document.getElementById("endereco").value = pessoasDb[index].endereco;
-  document.getElementById("email").value = pessoasDb[index].email;
+    document.getElementById("nome").value = pessoasDb[index].nome;
+    document.getElementById("idade").value = pessoasDb[index].idade;
+    document.getElementById("endereco").value = pessoasDb[index].endereco;
+    document.getElementById("email").value = pessoasDb[index].email;
 
-  document.getElementById("btnAtualizar").onclick = function () {
-    if (validaFormulario()) {
-      pessoasDb[index].nome = document.getElementById("nome").value;
-      pessoasDb[index].idade = document.getElementById("idade").value;
-      pessoasDb[index].endereco = document.getElementById("endereco").value;
-      pessoasDb[index].email = document.getElementById("email").value;
+    document.getElementById("btnAtualizar").onclick = function () {
+        if (validaFormulario()) {
+            pessoasDb[index].nome = document.getElementById("nome").value;
+            pessoasDb[index].idade = document.getElementById("idade").value;
+            pessoasDb[index].endereco =
+                document.getElementById("endereco").value;
+            pessoasDb[index].email = document.getElementById("email").value;
 
-      localStorage.setItem("pessoas", JSON.stringify(pessoasDb));
+            localStorage.setItem("pessoas", JSON.stringify(pessoasDb));
 
-      limpaFormulario();
-      mostraPessoas();
-      encerraAtualizacao();
-    }
-  };
+            limpaFormulario();
+            mostraPessoas();
+            encerraAtualizacao();
+        }
+    };
 }
 
 function encerraAtualizacao() {
-  document.getElementById("btnAdicionar").style.display = "block";
-  document.getElementById("deletar").style.display = "inline-block";
-  document.getElementById("atualizar").style.display = "inline-block";
-  document.getElementById("btnAtualizar").style.display = "none";
-  document.getElementById("btnCancelar").style.display = "none";
+    document.getElementById("btnAdicionar").style.display = "block";
+    document.getElementById("deletar").style.display = "inline-block";
+    document.getElementById("atualizar").style.display = "inline-block";
+    document.getElementById("btnAtualizar").style.display = "none";
+    document.getElementById("btnCancelar").style.display = "none";
 
-  limpaFormulario();
+    limpaFormulario();
 }
 
 function limpaFormulario() {
-  document.getElementById("nome").value = "";
-  document.getElementById("idade").value = "";
-  document.getElementById("endereco").value = "";
-  document.getElementById("email").value = "";
+    document.getElementById("nome").value = "";
+    document.getElementById("idade").value = "";
+    document.getElementById("endereco").value = "";
+    document.getElementById("email").value = "";
 }
 
 function getPessoasDoLocalStorage() {
-  return JSON.parse(localStorage.getItem("pessoas")) ?? [];
+    return JSON.parse(localStorage.getItem("pessoas")) ?? [];
 }
